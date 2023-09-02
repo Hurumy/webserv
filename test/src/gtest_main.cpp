2f9f399b1d5c3e220086e93e0a562b79c0bc8e2b
ShotaTanemura <shota0711tane@gmail.com>
2023-09-02 17:09:37 +0900

    WS01 Google unit test compiles src files of webserv and execute unit test with utest task

---
+++ test/src/gtest_main.cpp
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   gtest_main.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shtanemu <shtanemu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/02 16:33:21 by shtanemu          #+#    #+#             */
+/*   Updated: 2023/09/02 16:33:26 by shtanemu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <gtest/gtest.h>
+
+int	main(int argc, char *argv[]) {
+    ::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
