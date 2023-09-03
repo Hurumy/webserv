17e90d2936cbfe0bbb10f8dc4f0d72fcecbbee3a
ShotaTanemura <shota0711tane@gmail.com>
2023-09-03 16:54:38 +0900

    WS02 Include frequently used header files in webserv.hpp

---
+++ include/webserv.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   webserv.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shtanemu <shtanemu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/03 15:09:11 by shtanemu          #+#    #+#             */
+/*   Updated: 2023/09/03 15:41:00 by shtanemu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+# include <string>
+# include <iostream>
+# include <iomanip>
+# include <map>
+# include <algorithm>
+# include <sys/socket.h>
+# include <netinet/in.h>
+# include <netinet/ip.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <sys/types.h>
+# include <arpa/inet.h>
+# include <poll.h>
