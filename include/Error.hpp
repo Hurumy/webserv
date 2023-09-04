7970acaa56f31b2db78fdfd21c2587c79723e438
ShotaTanemura <shota0711tane@gmail.com>
2023-09-04 15:33:56 +0900

    WS02 Result class hold normal value or error value

---
+++ include/Error.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Error.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shtanemu <shtanemu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/04 15:03:22 by shtanemu          #+#    #+#             */
+/*   Updated: 2023/09/04 15:26:27 by shtanemu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include "Result.hpp"
+
+template <typename E>
+class Error {
+	public:
+		Error(E const &e) : e(e) {}
+		Error(Error const &sourceError) { *this = sourceError; }
+		Error & operator = (Error const &sourceError) {
+			if (this != &sourceError) {
+				e = sourceError.e;
+			}
+			return *this;
+		}
+		~Error() {}
+		template <typename T, typename V>
+		operator Result<T, V>() const { return Result<T, V>(e); }
+
+	private:
+		Error() {}
+
+		E e;
+};
---
+++ include/Ok.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Ok.hpp                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shtanemu <shtanemu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/04 15:02:38 by shtanemu          #+#    #+#             */
+/*   Updated: 2023/09/04 15:26:47 by shtanemu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include "Result.hpp"
+
+template <typename T>
+class Ok {
+	public:
+		Ok(T const &t) : t(t) {}
+		Ok(Ok const &sourceOk) { *this = sourceOk; }
+		Ok & operator = (Ok const &sourceOk) {
+			if (this != &sourceOk) {
+				t = sourceOk.t;
+			}
+			return *this;
+		}
+		~Ok() {}
+		template <typename V, typename E>
+		operator Result<V, E>() const { return Result<V, E>(t); }
+
+	private:
+		Ok() {}
+
+		T t;
+};
