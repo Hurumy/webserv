6912d4a53ef6f686e21c64d914096d46e514dae2
Hurumy <sarrendish@gmail.com>
2023-09-07 15:54:54 +0900

    いまいちうまく進められなかった。必要が生じ次第適宜追加しようと思います。

---
+++ src/read_req_header.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   read_req_header.cpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: komatsud <komatsud@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/07 14:52:27 by komatsud          #+#    #+#             */
+/*   Updated: 2023/09/07 15:08:32 by komatsud         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "webserv.hpp"
+#include "APayload.hpp"
+#include "Request.hpp"
+#include "Response.hpp"
+#include "Ok.hpp"
+#include "Error.hpp"
+#include "Result.hpp"
+
+
+Result<size_t, int>	readReqHeader(Request req, Result &res)
+{
+	//ifを並べて判定する
+	if ()
+}
+
