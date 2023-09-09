06718a66d1876d04714d1c3b70bf2c95bc96bf58
ShotaTanemura <shota0711tane@gmail.com>
2023-09-09 13:17:03 +0900

    WS05 Unit test tests SocketHandler's constructor recieves SSocket and hold it

---
+++ test/src/SocketHandler_test.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   SocketHandler_test.cpp                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shtanemu <shtanemu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/09 12:14:49 by shtanemu          #+#    #+#             */
+/*   Updated: 2023/09/09 12:30:37 by shtanemu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <gtest/gtest.h>
+
+#include "SocketHandler.hpp"
+
+TEST(SocketHandlerTest, constructorTest) {
+	std::vector<SSocket> sources;
+
+	sources.push_back(SSocket(8080, IPV4, 100));
+	sources.push_back(SSocket(8000, IPV4, 100));
+	sources.push_back(SSocket(4040, IPV4, 100));
+	SocketHandler socketHandler(sources);
+	std::vector<SSocket> const &ssockets = socketHandler.getSSockets();
+	ASSERT_EQ(ssockets.at(0).getPort(), 8080);
+	ASSERT_EQ(ssockets.at(1).getPort(), 8000);
+	ASSERT_EQ(ssockets.at(2).getPort(), 4040);
+}
