54f925cb27354494542ff7532e9d45f29fb75c9c
ShotaTanemura <shota0711tane@gmail.com>
2023-09-09 13:34:58 +0900

    WS05 addCSocket add CSocket instance to csockets

--- include/SocketHandler.hpp
+++ include/SocketHandler.hpp
@@ -6,22 +6,26 @@
 /*   By: shtanemu <shtanemu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/09 12:12:24 by shtanemu          #+#    #+#             */
-/*   Updated: 2023/09/09 12:31:16 by shtanemu         ###   ########.fr       */
+/*   Updated: 2023/09/09 13:33:31 by shtanemu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <vector>
 
 #include "SSocket.hpp"
+#include "CSocket.hpp"
 
 class SocketHandler {
 	private:
 		SocketHandler();
 
 		std::vector<SSocket> ssockets;
+		std::vector<CSocket> csockets;
 	protected:
 	public:
 		SocketHandler(std::vector<SSocket> &_ssockets);
 
 		std::vector<SSocket> const &getSSockets() const;
+		std::vector<CSocket> const &getCSockets() const;
+		void addCSocket(CSocket const &_csocket);
 };
--- src/SocketHandler.cpp
+++ src/SocketHandler.cpp
@@ -6,7 +6,7 @@
 /*   By: shtanemu <shtanemu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/09 12:26:40 by shtanemu          #+#    #+#             */
-/*   Updated: 2023/09/09 12:32:19 by shtanemu         ###   ########.fr       */
+/*   Updated: 2023/09/09 13:33:51 by shtanemu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,18 @@
 
 #include <vector>
 #include "SSocket.hpp"
+#include "CSocket.hpp"
 
 SocketHandler::SocketHandler(std::vector<SSocket> &_ssockets) : ssockets(_ssockets) {}
 
 std::vector<SSocket> const &SocketHandler::getSSockets() const {
 	return ssockets;
+}
+
+std::vector<CSocket> const &SocketHandler::getCSockets() const {
+	return csockets;
+}
+
+void SocketHandler::addCSocket(CSocket const &_csocket) {
+	csockets.push_back(_csocket);
 }
