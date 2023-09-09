dcab6f7b1aa390343c29d8d0232ed6c919e6a4c2
ShotaTanemura <shota0711tane@gmail.com>
2023-09-09 14:51:33 +0900

    WS05 Unit test tests setRevents()

--- test/src/SocketHandler_test.cpp
+++ test/src/SocketHandler_test.cpp
@@ -6,7 +6,7 @@
 /*   By: shtanemu <shtanemu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/09 12:14:49 by shtanemu          #+#    #+#             */
-/*   Updated: 2023/09/09 14:17:03 by shtanemu         ###   ########.fr       */
+/*   Updated: 2023/09/09 14:50:18 by shtanemu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ TEST(SocketHandlerTest, constructorTest) {
 	sources.push_back(SSocket(8080, IPV4, 100));
 	sources.push_back(SSocket(8000, IPV4, 100));
 	sources.push_back(SSocket(4040, IPV4, 100));
-	SocketHandler socketHandler(sources);
+	SocketHandler socketHandler(sources, 1000);
 	std::vector<SSocket> const &ssockets = socketHandler.getSSockets();
 	ASSERT_EQ(ssockets.at(0).getPort(), 8080);
 	ASSERT_EQ(ssockets.at(1).getPort(), 8000);
@@ -30,17 +30,33 @@ TEST(SocketHandlerTest, constructorTest) {
 	ASSERT_EQ(csockets.at(0).getRevents(), 0);
 }
 
-
 TEST(SocketHandlerTest, pollfdsTest) {
 	std::vector<SSocket> sources;
 
 	sources.push_back(SSocket(8080, IPV4, 100));
 	sources.push_back(SSocket(8000, IPV4, 100));
 	sources.push_back(SSocket(4040, IPV4, 100));
-	SocketHandler socketHandler(sources);
+	SocketHandler socketHandler(sources, 1000);
 	socketHandler.createPollfds();
 	std::vector<SSocket> const &ssockets = socketHandler.getSSockets();
 	std::vector<struct pollfd> const &pollfds = socketHandler.getPollfds();
 	ASSERT_EQ(pollfds.at(0).fd, ssockets.at(0).getSockfd());
 	ASSERT_EQ(pollfds.at(0).events, POLLIN | POLLOUT | POLLHUP);
+}
+
+TEST(SocketHandlerTest, setReventsTest) {
+	std::vector<SSocket> sources;
+
+	sources.push_back(SSocket(8080, IPV4, 100));
+	sources.push_back(SSocket(8000, IPV4, 100));
+	sources.push_back(SSocket(4040, IPV4, 100));
+	SocketHandler socketHandler(sources, 1000);
+	socketHandler.createPollfds();
+	std::vector<SSocket> const &ssockets = socketHandler.getSSockets();
+	std::vector<struct pollfd> const &pollfds = socketHandler.getPollfds();
+	ASSERT_EQ(pollfds.at(0).fd, ssockets.at(0).getSockfd());
+	ASSERT_EQ(pollfds.at(0).events, POLLIN | POLLOUT | POLLHUP);
+	ASSERT_TRUE(socketHandler.setRevents());
+	std::vector<struct pollfd> const &resutl_pollfds = socketHandler.getPollfds();
+	ASSERT_EQ(resutl_pollfds.at(0).fd, ssockets.at(0).getSockfd());
 }
