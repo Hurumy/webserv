8c1fa68a60054d20a2113adc5cc8e96f2978a426
Hurumy <sarrendish@gmail.com>
2023-09-11 13:41:10 +0900

    途中経過

--- src/makeConf.cpp
+++ src/makeConf.cpp
@@ -6,7 +6,7 @@
 /*   By: komatsud <komatsud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/08 11:37:10 by komatsud          #+#    #+#             */
-/*   Updated: 2023/09/09 15:02:26 by komatsud         ###   ########.fr       */
+/*   Updated: 2023/09/11 13:16:00 by komatsud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,51 @@
 #include "Error.hpp"
 
 std::vector<std::string> lineSpliter(std::string origin, std::string delim);
+
+std::string	cutOffStr(std::string &origin, std::string start, std::string end)
+{
+	size_t		startpos;
+	size_t		endpos;
+	size_t		offset = 0;
+	std::string	prev;
+	std::string	next;
+	std::string	result = origin;
+
+	while (1)
+	{
+		startpos = origin.find(start, offset);
+		if (startpos == std::string::npos)
+			break;
+		endpos = origin.find(end, startpos);
+		if (endpos == std::string::npos)
+			break;
+		prev = origin.substr(0, startpos);
+		next = origin.substr(endpos, origin.size());
+		result = prev;
+		result += next;
+		offset = startpos + 1;
+	}
+	origin = result;
+	return (result);
+}
+
+std::string	replaceStr(std::string &origin, std::string search, std::string replace)
+{
+	size_t			pos = 0;
+	size_t			offset = 0;
+	const size_t	searchlen = search.length();
+	const size_t	replen = replace.length();
+
+	while (pos != std::string::npos || search.empty() == false)
+	{
+		pos = origin.find(search, offset);
+		if (pos == std::string::npos)
+			break;
+		origin.replace(pos, searchlen, replace);
+		offset = pos + replen;
+	}
+	return (origin);
+}
 
 Result<std::string, bool>	openAndReadConf(std::string filepath)
 {
@@ -184,10 +229,72 @@ static int		isServerSetting(std::string raw)
 		return (0);
 }
 
-static int	checkSettings(Config conf, std::vector<std::string>	oneline)
+static int	readListen(Config &conf, std::string	oneline)
 {
-	for (size_t i = 0; i < oneline.size(); i ++)
-		std::cout << RED ": " << oneline.at(i) << RESET << std::endl;
+	size_t	pos;
+
+	status = oneline.find(":", 0);
+	if (status == std::string::npos)
+	{
+		std::stringstream	ss;
+		int					port;
+		
+		if (oneline.size() != 2 && oneline.size() != 3)
+		{
+			return (-1);
+		}
+		ss << oneline.at(1);
+		ss >> port;
+		if (!(1 <= port && port <= 65535)
+			return (-1);
+		conf.setPort(port);
+	}
+	else
+	{
+		std::vector<std::string>	div;
+		int							port;
+		std::stringstream			ss;
+
+		if (oneline.size() != 2 && oneline.size() != 3)
+			return (-1);
+		div = lineSpliter(oneline.at(1), ":");
+		
+	}
+}
+
+static int	checkSettings(Config conf, std::string	oneline)
+{
+	std::vector<std::string>	lines;
+
+	lines = lineSpliter(oneline, " ");
+	for (size_t i = 0; i < lines.size(); i ++)
+	{
+		std::cout << YELLOW ": " << lines.at(i) << RESET << std::endl;		
+		
+		if(lines.at(i).empty == false)
+		{
+			if (lines.at(i) == "listen")
+			{
+				if (i + 1 >= lines.size())	
+					return (-1);	
+				
+			}
+			else if (lines.at(i) == "server_name")
+			{}
+			else if (lines.at(i) == "root")
+			{}
+			else if (lines.at(i) == "location")
+			{}
+			else if (lines.at(i) == "error_page")
+			{}
+			else if (lines.at(i) == "return")
+			{}
+		}
+
+		//ヘッダーが正しいか判別
+		//空行は飛ばす
+		//内容があれば詰める
+	}
 	conf.setPort(oneline.size());
 	return 0;
 }
@@ -198,16 +305,26 @@ static Config	getConf(std::string raw)
 	std::vector<std::string>	oneline;
 	Config						conf;
 
-	raw.erase(std::remove(raw.begin(), raw.end(), '\n'), raw.end());
-	raw.erase(std::remove(raw.begin(), raw.end(), '\r'), raw.end());
-	std::regex_replace(raw, std::regex("\t"), "    ");
+	//delete&ignore comments
+	cutOffStr(raw, "#", "\n");
 
+	//interpret spaces as " "
+	replaceStr(raw, "\n", " ");
+	replaceStr(raw, "\r", " ");
+	replaceStr(raw, "\t", " ");
+	replaceStr(raw, "\v", " ");
+	replaceStr(raw, "\f", " ");
+	replaceStr(raw, "{", ";{;");
+	replaceStr(raw, "}", ";};");
+
+	//cut string by ";"
 	lines = lineSpliter(raw, ";");
 
+	//get confs
 	for (size_t i = 0; i < lines.size(); i ++)
 	{
-		oneline = lineSpliter(lines.at(i), " ");
-		checkSettings(conf, oneline);
+		checkSettings(conf, lines.at(i));
+		std::cout << RED "/" << RESET << std::endl;
 	}
 	return (conf);
 }
