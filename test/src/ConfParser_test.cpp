b108cea2d2865384348a512bba8acc04ae41c615
Hurumy <sarrendish@gmail.com>
2023-09-15 12:31:42 +0900

    tmp

--- test/src/ConfParser_test.cpp
+++ test/src/ConfParser_test.cpp
@@ -6,7 +6,7 @@
 /*   By: komatsud <komatsud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/14 16:26:58 by komatsud          #+#    #+#             */
-/*   Updated: 2023/09/14 18:02:32 by komatsud         ###   ########.fr       */
+/*   Updated: 2023/09/15 10:50:11 by komatsud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,15 +21,32 @@
 
 #define	CONF_FILE_PATH "testconfs/simple.conf"
 
-TEST(ConfigParserTest, listenTest)
+TEST(ConfigParserTest, pPortTest)
 {
 	std::vector<Config>	tmp;
-	int					expected_1(80);
+	int					expected_1(8660);
 	int					expected_2(80);
 	int					expected_3(8080);
+	int					expected_4(80);
 	
 	Result<std::vector<Config>, bool> res = parseConf(CONF_FILE_PATH);
 	tmp = res.getOk();
-	
-
+	ASSERT_EQ(tmp.at(0).getPort().at(0), expected_1);
+	ASSERT_EQ(tmp.at(0).getPort().at(1), expected_2);
+	ASSERT_EQ(tmp.at(1).getPort().at(0), expected_3);	
+	ASSERT_EQ(tmp.at(1).getPort().at(1), expected_4);
 }
+
+TEST(ConfigParserTest, pIPTest)
+{
+	std::vector<Config>	tmp;
+	std::string			expected_1("111.108.92.125");
+	std::string			expected_2("::");
+	
+	Result<std::vector<Config>, bool> res = parseConf(CONF_FILE_PATH);
+	tmp = res.getOk();
+	ASSERT_EQ(tmp.at(0).getIpAddress().at(0), expected_1);
+	ASSERT_EQ(tmp.at(0).getIpAddress().at(1), expected_2);
+}
+
+
