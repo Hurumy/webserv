2b4ed1d3ca39dabf8df46eda25ef60e6b35ef8ee
ShotaTanemura <shota0711tane@gmail.com>
2023-09-18 15:25:19 +0900

    WS05 Request manages its condition to handle I/O Multiplexing

--- include/Request.hpp
+++ include/Request.hpp
@@ -3,30 +3,56 @@
 /*                                                        :::      ::::::::   */
 /*   Request.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: komatsud <komatsud@student.42.fr>          +#+  +:+       +#+        */
+/*   By: shtanemu <shtanemu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/04 16:59:27 by komatsud          #+#    #+#             */
-/*   Updated: 2023/09/05 10:58:43 by komatsud         ###   ########.fr       */
+/*   Updated: 2023/09/18 00:42:33 by shtanemu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
+#include <vector>
+#include <string>
+
+#include "CSocket.hpp"
 #include "webserv.hpp"
 #include "APayload.hpp"
 
+
 class Request: public virtual APayload
 {
-	private:
-		std::string	url;
-		std::string method;
-	protected:
 	public:
+		enum tag {
+			REQLINE,
+			HEADER,
+			BODY,
+		};
+		Request();
+
 		const std::string	getLines() const;
 		bool				setUrl(std::string url);
 		std::string const	getUrl(void) const;
 		bool				setMethod(std::string method);
 		std::string const	getMethod(void) const;
+		const tag &getPhase() const;
+		bool loadPayload(CSocket &csocket);
+		bool loadHeader(CSocket &csocket);
+	protected:
+	private:
+		std::string	url;
+		std::string method;
+		std::size_t contentLength;
+		bool isCompleteHeader;
+		static std::vector<std::string> const methods;
+		tag phase;
+
+		static std::vector<std::string> initMethods();
+		bool setRequestLine(std::string const &line);
+		bool loadRequestLine(CSocket &csocket);
+		bool setHeader(std::string const &line);
+		bool loadHeader(CSocket const &csocket);
+		bool isMethod(std::string const &word);
+		bool isURL(std::string const &word);
+		bool isVersion(std::string const &word);
 };
-
-
--- src/Request.cpp
+++ src/Request.cpp
@@ -3,21 +3,50 @@
 /*                                                        :::      ::::::::   */
 /*   Request.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: komatsud <komatsud@student.42.fr>          +#+  +:+       +#+        */
+/*   By: shtanemu <shtanemu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/04 16:54:10 by komatsud          #+#    #+#             */
-/*   Updated: 2023/09/05 11:05:44 by komatsud         ###   ########.fr       */
+/*   Updated: 2023/09/18 15:17:47 by shtanemu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Request.hpp"
 
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <sstream>
+
+std::vector<std::string> const Request::methods = Request::initMethods();
+
+std::vector<std::string> Request::initMethods() {
+	std::vector<std::string> _methods;
+	_methods.push_back("GET");
+	_methods.push_back("HEAD");
+	_methods.push_back("POST");
+	_methods.push_back("PUT");
+	_methods.push_back("DELETE");
+	_methods.push_back("CONNECT");
+	_methods.push_back("OPTIONS");
+	_methods.push_back("TRACE");
+	return _methods;
+}
+
+Request::Request() : contentLength(0), isCompleteHeader(false), phase(Request::REQLINE) {}
+
 const std::string Request::getLines() const {
 	std::string line;
 
-	line += this->version;
-	line += this->body;
+	line += this->method;
+	line += " ";
 	line += this->url;
+	line += " ";
+	line += this->version;
+	line += "\n";
+	for (std::map<std::string, std::string>::const_iterator iter = header.begin(); iter != header.end(); ++iter) {
+		line += iter->first + ": " + iter->second + "\n";
+	}
+	line += this->body;
 	return (line);
 }
 
@@ -34,6 +63,238 @@ bool Request::setMethod(std::string method) {
 }
 
 std::string const Request::getMethod(void) const { return (this->method); }
+
+bool Request::loadPayload(CSocket &csocket) {
+	while (true) {
+		switch (phase) {
+			case Request::REQLINE:
+				if (loadRequestLine(csocket) == true) {
+					phase = Request::HEADER;
+				} else {
+					return false;
+				}
+				break;
+			case Request::HEADER:
+				if (loadHeader(csocket) == true) {
+					if (csocket._getData().compare(0, 2, "\r\n") == 0) {
+						std::map<std::string, std::string>::iterator clengthiter = header.find("Content-Length");
+						if (clengthiter != header.end()) {
+							std::stringstream ss(clengthiter->second);
+							ss >> contentLength;
+							return true;
+						} else {
+							// error handling
+						}
+						phase = Request::BODY;
+					}
+				} else {
+					return false;
+				}
+				break;
+			case Request::BODY:
+				body.append(csocket._getData(), contentLength);
+				csocket.eraseData(contentLength);
+				phase = Request::REQLINE;
+				csocket.setPhase(CSocket::PASS);
+				std::clog << getLines() << std::endl;
+				return true;
+				break;
+		}
+	}
+	return true;
+}
+
+bool Request::loadRequestLine(CSocket &csocket) {
+	std::istringstream iss(csocket.getDataLine());
+	std::string _method;
+	std::string _url;
+	std::string _version;
+
+	iss >> _method;
+	iss >> _url;
+	iss >> _version;
+	if (isMethod(_method) == false) {
+		return false;
+	}
+	if (isURL(_url) == false) {
+		return false;
+	}
+	if (isVersion(_version) == false) {
+		return false;	
+	}
+	setMethod(_method);
+	setUrl(_url);
+	setVersion(_version);
+	csocket.popDataLine();
+	return true;
+}
+
+bool Request::loadHeader(CSocket &csocket) {
+	std::istringstream iss(csocket.getDataLine());
+	std::string key;
+	std::string value;
+
+	iss >> key;
+	if (key.empty() == false) {
+		key.resize(key.size() - 1);
+	} else {
+		return false;
+	}
+	iss >> value;
+	if (value.empty() == false) {
+		header[key] = value;
+	} else {
+		return false;
+	}
+	while (iss >> value) {
+		header[key].append(" " + value);
+	}
+	csocket.popDataLine();
+	return true;
+}
+
+// bool Request::loadPayload(CSocket &csocket) {
+// 	std::string line;
+
+// 	// should check about content-length
+// 	if ((csocket._getData().find("\r\n") == std::string::npos) && (url.empty() || method.empty() || version.empty() || isCompleteHeader == false)) {
+// 		return false;
+// 	}
+// 	while (csocket._getData().empty() == false && contentLength >= body.size()) {
+// 		if ((csocket._getData().find("\r\n") == std::string::npos) && (url.empty() || method.empty() || version.empty() || isCompleteHeader == false)) {
+// 			return false;
+// 		}
+// 		line = csocket.getDataLine();
+// 		if (url.empty() || method.empty() || version.empty()) {
+// 			if (setRequestLine(line) == false) {	
+// 				// error handling
+// 			} else {
+// 				csocket.popDataLine();
+// 			}
+// 		} else if (isCompleteHeader == false) {
+// 			if (setHeader(line) == false) {
+// 				// error handling
+// 			} else {
+// 				csocket.popDataLine();
+// 			}
+// 		} else if (contentLength > body.size()) {
+// 			body.append(line);
+// 			csocket.popDataLine();
+// 		}
+// 	}
+// 	return true;
+// }
+
+// bool Request::loadHeader(CSocket &csocket) {
+// 	std::string line;
+
+// 	// should check about content-length
+// 	if ((csocket._getData().find("\r\n") == std::string::npos) && (url.empty() || method.empty() || version.empty() || isCompleteHeader == false)) {
+// 		return false;
+// 	}
+// 	while (csocket._getData().empty() == false && contentLength >= body.size()) {
+// 		if ((csocket._getData().find("\r\n") == std::string::npos) && (url.empty() || method.empty() || version.empty() || isCompleteHeader == false)) {
+// 			return false;
+// 		}
+// 		line = csocket.getDataLine();
+// 		if (url.empty() || method.empty() || version.empty()) {
+// 			if (setRequestLine(line) == false) {	
+// 				// error handling
+// 			} else {
+// 				csocket.popDataLine();
+// 			}
+// 		} else if (isCompleteHeader == false) {
+// 			if (setHeader(line) == false) {
+// 				// error handling
+// 			} else {
+// 				csocket.popDataLine();
+// 			}
+// 		} else if (contentLength > body.size()) {
+// 			body.append(line);
+// 			csocket.popDataLine();
+// 		}
+// 	}
+// 	return true;
+// }
+
+bool Request::isMethod(std::string const &word) {
+	return std::find(methods.begin(), methods.begin(), word) != methods.end();
+}
+
+bool Request::isURL(std::string const &word) {
+	return word.empty() == false;
+}
+
+bool Request::isVersion(std::string const &word) {
+	return word.empty() == false;
+}
+
+bool Request::setRequestLine(std::string const &line) {
+	std::istringstream iss(line);
+	std::string _method;
+	std::string _url;
+	std::string _version;
+
+	iss >> _method;
+	iss >> _url;
+	iss >> _version;
+	if (isMethod(_method) == false) {
+		return false;
+	}
+	if (isURL(_url) == false) {
+		return false;
+	}
+	if (isVersion(_version) == false) {
+		return false;	
+	}
+	setMethod(_method);
+	setUrl(_url);
+	setVersion(_version);
+	return true;
+}
+
+bool Request::setHeader(std::string const &line) {
+	std::istringstream iss(line);
+	std::string key;
+	std::string value;
+
+	iss >> key;
+	if (key.empty() == false) {
+		key.resize(key.size() - 1);
+	} else {
+		return false;
+	}
+	iss >> value;
+	if (value.empty() == false) {
+		header[key] = value;
+	} else {
+		return false;
+	}
+	while (iss >> value) {
+		header[key].append(" " + value);
+	}
+	return true;
+}
+
+const Request::tag &Request::getPhase() const {
+	return phase;
+}
+
+// bool Request::setHeader(std::string const &line) {
+// 	std::istringstream iss(line);
+// 	std::string key;
+// 	std::string value;
+
+// 	iss >> key;
+// 	iss >> value;
+// 	if ((key.empty() == false) && (value.empty() == false)) {
+// 		key.resize(key.size() - 1);
+// 		header[key] = value;
+// 	} else {
+// 		return false;
+// 	}
+// 	return true;
+// }
 
 // test code
 /*
