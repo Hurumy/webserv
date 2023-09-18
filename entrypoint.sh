25f2268dc37ad45d8b0707beb9561bb4ae333232
ShotaTanemura <shota0711tane@gmail.com>
2023-09-18 17:15:04 +0900

    WS05 entrypoint.sh installs necessary app and libraries

---
+++ entrypoint.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -e
+
+echo "${0}: install telnet."
+apt install telnet
+status=$?
+if [ $status -ne 0 ]; then
+  echo "Failed to install telnet: $status"
+  exit $status
+fi
+
+echo "${0}: install siege."
+apt install siege
+status=$?
+if [ $status -ne 0 ]; then
+  echo "Failed to install siege: $status"
+  exit $status
+fi
+
+echo "${0}: install robotframework."
+pip install robotframework
+status=$?
+if [ $status -ne 0 ]; then
+  echo "Failed to install robotframework: $status"
+  exit $status
+fi
+
+echo "${0}: compile sources."
+make re
+status=$?
+if [ $status -ne 0 ]; then
+  echo "Failed to compile: $status"
+  exit $status
+fi
+
+echo "${0}: running webserv."
+./webserv
+status=$?
+if [ $status -ne 0 ]; then
+  echo "Failed to start webserv: $status"
+  exit $status
+fi
