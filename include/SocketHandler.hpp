d312e8444bddfef57f632835a9accff45603c437
ShotaTanemura <shota0711tane@gmail.com>
2023-09-18 15:26:03 +0900

    WS05 SocketHandler handles Request and CSocket properly for i/o multiplexing

--- include/SocketHandler.hpp
+++ include/SocketHandler.hpp
@@ -6,7 +6,7 @@
 /*   By: shtanemu <shtanemu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/09 12:12:24 by shtanemu          #+#    #+#             */
-/*   Updated: 2023/09/12 12:34:51 by shtanemu         ###   ########.fr       */
+/*   Updated: 2023/09/18 14:52:34 by shtanemu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 #include <vector>
 #include <map>
 
+#include "Request.hpp"
 #include "Result.hpp"
 #include "SSocket.hpp"
 #include "CSocket.hpp"
@@ -29,12 +30,15 @@ class SocketHandler {
 		std::vector<SSocket> ssockets;
 		std::vector<CSocket> csockets;
 		std::vector<struct pollfd> pollfds;
+		std::map<int, Request> requests;
+
 	protected:
 	public:
 		SocketHandler(std::vector<SSocket> &_ssockets, int const _timeout);
 
 		bool initAllSSockets();
 		bool closeAllSSockets();
+		bool removeRequest(int const csockfd);
 		bool removeClosedCSockets();
 		std::vector<SSocket> const &getSSockets() const;
 		std::vector<CSocket> const &getCSockets() const;
@@ -46,5 +50,8 @@ class SocketHandler {
 		bool setRevents();
 		bool recieveCSockets();
 		Result<std::map<int, std::string>, bool> getDataMap() const;
+		std::map<int, Request> getRequestsMap() const;
+		bool recvCSocketsData();
 		bool sendDataMap(std::map<int, std::string> const &dataMap) const;
+		bool loadRequests();
 };
