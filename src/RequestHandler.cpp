821a88074d4a107b85dd7d63edd11d6d0a13548e
Hurumy <sarrendish@gmail.com>
2023-09-21 18:35:37 +0900

    RequestHandlerクラスの、エラーページをセットする関数と、ファイルパスの中身を読み込んでBodyにセットしContent-lengthを設定する関数を作りました。

--- src/RequestHandler.cpp
+++ src/RequestHandler.cpp
@@ -6,7 +6,7 @@
 /*   By: komatsud <komatsud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 17:32:21 by komatsud          #+#    #+#             */
-/*   Updated: 2023/09/21 17:25:21 by komatsud         ###   ########.fr       */
+/*   Updated: 2023/09/21 18:32:01 by komatsud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -88,6 +88,86 @@ Result<int, bool>	RequestHandler::routeMethod()
 		return Error<bool>(false);
 	}
 	return Error<bool>(false);
+}
+
+Result<std::string, bool>	RequestHandler::_openFile(std::string filename)
+{
+	int 				fd;
+	unsigned long long	bodysize = 0;
+	int 				status = 1;
+	std::string 		body;
+	char 				buf[FILE_READ_SIZE];
+
+	// open
+	fd = open(filename.c_str(), O_RDONLY);
+	if (fd == -1 && errno == ENOENT) {
+		res.setStatus(404);
+		res.setStatusMessage("Not Found");
+		return Error<bool>(false);
+	} else if (fd == -1 && errno == EACCES) {
+		res.setStatus(403);
+		res.setStatusMessage("Forbidden");
+		return Error<bool>(false);
+	}
+
+	// read
+	while (status > 0) {
+		status = read(fd, buf, FILE_READ_SIZE);
+		buf[status] = '\0';
+		if (status != -1)
+		{
+			body += buf;
+			bodysize += status;
+		}
+	}
+	close(fd);
+
+	if (status == -1) {
+		res.setStatus(500);
+		res.setStatusMessage("Internal Server Error");
+		return Error<bool>(false);
+	}
+
+	//Bodyの読み込みが成功していたら、bodysizeとBodyをセットして返る
+	std::stringstream		ss;
+	std::string				length;
+	ss << bodysize;
+	ss >> length;
+	res.addHeader("Content-Length", length);
+	res.setBody(body);
+	return Ok<std::string>(body);
+}
+
+void		RequestHandler::setErrorPageBody()
+{
+	unsigned int				prevstatus = res.getStatus();
+	Result<std::string, bool>	res_1 = configs.at(confnum).getErrorPages(res.getStatus());
+	
+	if (res_1.isOK() == false)
+		return ;
+
+	std::string					filename = res_1.getOk();
+
+	//bodyをセットする。成功したら抜けるループ
+	//bodyのセットに失敗した場合は、失敗した後のステータスが変わっていればもう一度ファイルを検索する
+	//変わっていなければBodyなしでヘッダだけ送付する
+	while (1)
+	{
+		Result<std::string, bool> res_2 = _openFile(filename);
+		if (res_2.isOK() == true)
+			break ;
+		else if (res_2.isOK() == false && prevstatus != res.getStatus())
+		{
+			prevstatus = res.getStatus();
+			Result<std::string, bool>	res_3 = configs.at(confnum).getErrorPages(res.getStatus());
+			if (res_3.isOK() == false)
+				break ;
+			filename = res_3.getOk();
+		}
+		else
+			break ;
+	}
+	return ;
 }
 
 Response	RequestHandler::getResponse()
--- test/src/RequestHandler_test.cpp
+++ test/src/RequestHandler_test.cpp
@@ -6,7 +6,7 @@
 /*   By: komatsud <komatsud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 18:17:48 by komatsud          #+#    #+#             */
-/*   Updated: 2023/09/21 12:57:31 by komatsud         ###   ########.fr       */
+/*   Updated: 2023/09/21 18:34:28 by komatsud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -161,4 +161,35 @@ TEST(RequestHandlerTest, getResponseTest)
 	ASSERT_EQ(result_1.isOK(), expected);
 	ASSERT_EQ(handler.getResponse().getStatus(), expected_status);
 	ASSERT_EQ(handler.getResponse().getStatusMessage(), expected_string);
+}
+
+TEST(RequestHandlerTest, setErrorPageBodyTest_Error_HTTPVersion)
+{
+	Result<std::vector<Config>, bool> res = parseConf(CONF_FILE_PATH);
+	std::vector<Config>			tmp = res.getOk();
+	Request						req;
+	bool						expected(false);
+	unsigned int				expected_status(505);
+	std::string					expected_string("HTTP Version Not Supported");
+	std::string					expected_body("<H1>HTTP 505 HTTP Version Not Supported</H1>\n\n<p>This webserv supports only HTTP/1.1...</p>");
+	std::string					expected_content_length("91");
+
+	req.setVersion("HTTP/2.0");
+	req.setMethod("GET");
+	req.addHeader("Host", "_");
+
+	RequestHandler handler = RequestHandler(tmp, req);
+	handler.searchMatchHost();
+	Result<int, bool> result_1 = handler.checkRequiedHeader();
+	ASSERT_EQ(result_1.isOK(), expected);
+
+	//errorPageBody
+	handler.setErrorPageBody();
+	Result<std::string, bool>	res_1 = handler.getResponse().getHeader("Content-Length");
+	std::string					content_len = res_1.getOk();
+
+	ASSERT_EQ(handler.getResponse().getStatus(), expected_status);
+	ASSERT_EQ(handler.getResponse().getStatusMessage(), expected_string);
+	ASSERT_EQ(handler.getResponse().getBody(), expected_body);
+	ASSERT_EQ(content_len, expected_content_length);
 }
