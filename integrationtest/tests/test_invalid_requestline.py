fbf7ceb7086ae59b49652edd5a2e3f61b71398f5
ShotaTanemura <shota0711tane@gmail.com>
2023-10-04 23:32:14 +0900

    WS06 Add test cases

--- integrationtest/tests/test_invalid_requestline.py
+++ integrationtest/tests/test_invalid_requestline.py
@@ -7,17 +7,16 @@ load_dotenv()
 HOST_NAME = os.getenv('HOST_NAME')
 PORT = os.getenv('PORT')
 
-class TestGetRequest(unittest.TestCase):
+class TestInvalidRequestLine(unittest.TestCase):
 	def setUp(self):
 		self.client = Ipv4Client(HOST_NAME, int(PORT))
 
-	def test_not_found(self):
+	def test_extra_word(self):
 		print('\n===========================')
-		print('TEST: test not found')
+		print('TEST: test extra word')
 		print('===========================\n')
-		self.client.send('GET /nosuchpage HTTP/1.1\r\nHost: ' + HOST_NAME + ':' + PORT + '\r\n\r\n')
+		self.client.send('GET /nosuchpage HTTP/1.1 thisisextra\r\n')
 		self.client.recv()
 		self.client.close()
 		print('Response:\n', self.client.response)
 		print('Status: ', self.client.status)
-		self.assertEqual(self.client.status, '404')
