b426e2b3c08808d71f92badbea45a520ebe64e9e
ShotaTanemura <shota0711tane@gmail.com>
2023-10-17 20:21:07 +0900

    WS07 Set remote address when accepts client socket

--- src/SocketHandler.cpp
+++ src/SocketHandler.cpp
@@ -6,7 +6,7 @@
 /*   By: shtanemu <shtanemu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/09 12:26:40 by shtanemu          #+#    #+#             */
-/*   Updated: 2023/10/17 16:35:28 by shtanemu         ###   ########.fr       */
+/*   Updated: 2023/10/17 20:06:23 by shtanemu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -196,7 +196,7 @@ bool SocketHandler::recieveCSockets() {
 				return false;
 			}
 			fcntl(sockfd, F_SETFL, O_NONBLOCK, FD_CLOEXEC);
-			csockets.push_back(CSocket(sockfd));
+			csockets.push_back(CSocket(sockfd, s_addr.sin_addr.s_addr));
 		}
 	}
 	return true;
