a58edd943e1fc610c06f9be4e854ee55e0ba0ce0
Hurumy <sarrendish@gmail.com>
2023-10-25 11:19:54 +0900

    Googletestを実行した時、Postディレクトリにファイルが一つずつ増えていくようになっていたのを修正しました

--- test/src/MethodDelete_test.cpp
+++ test/src/MethodDelete_test.cpp
@@ -6,7 +6,7 @@
 /*   By: komatsud <komatsud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 14:23:41 by komatsud          #+#    #+#             */
-/*   Updated: 2023/09/28 11:50:33 by komatsud         ###   ########.fr       */
+/*   Updated: 2023/10/25 11:19:31 by komatsud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,33 @@ TEST(MethodDeleteTest, deletePostedFileTest) {
 	req.setMethod("DELETE");
 	req.addHeader("Host", "_");
 	req.setUrl("/post/0");
+
+	RequestHandler handler = RequestHandler(tmp, req);
+	handler.searchMatchHost();
+	Result<int, bool> result_1 = handler.checkRequiedHeader();
+	ASSERT_EQ(result_1.isOK(), expected);
+
+	handler.routeMethod();
+
+	ASSERT_EQ(handler.getResponse().getStatus(), expected_status);
+	ASSERT_EQ(handler.getResponse().getStatusMessage(), expected_string);
+	ASSERT_EQ(handler.getResponse().getHeader("Content-Length").isOK(),
+			  expected_is_there_content_len);
+}
+
+TEST(MethodDeleteTest, deletePostedFile2Test) {
+	Result<std::vector<Config>, bool> res = parseConf(CONF_FILE_PATH);
+	std::vector<Config> tmp = res.getOk();
+	Request req;
+	bool expected(true);
+	unsigned int expected_status(204);
+	std::string expected_string("No Content");
+	bool expected_is_there_content_len(true);
+
+	req.setVersion("HTTP/1.1");
+	req.setMethod("DELETE");
+	req.addHeader("Host", "_");
+	req.setUrl("/post/1");
 
 	RequestHandler handler = RequestHandler(tmp, req);
 	handler.searchMatchHost();
