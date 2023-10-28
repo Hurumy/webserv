be4a8377e52ef6d9125263577707de7062851ea0
ShotaTanemura <shota0711tane@gmail.com>
2023-10-28 17:36:26 +0900

    WS08 Print all environmental variables for CGI script

--- content/test.py
+++ content/test.py
@@ -1,1 +1,21 @@
-print("Hello, Python CGI")
+import os
+
+# document response
+print("Content-Type: text/plain")
+print("X_TEST00: test")
+print("X_TEST01: test")
+print("X_TEST02: test")
+print("")
+print("Hello, world.")
+print("This is body of CGI.")
+
+for name, value in os.environ.items():
+    print("{0}: {1}".format(name, value))
+
+# local redirect response
+# print("Location: /you/me/they/are")
+
+# client redirect response with document
+# print("Location: /you/me/they/are")
+# print("Status: 200")
+# print("Hello, Python CGI")
