4ba2599a19ec57d7e55c9ea0b3d8343db2376498
ShotaTanemura <shota0711tane@gmail.com>
2023-11-07 12:13:33 +0900

    WS08 Test case insensitive Content type header

---
+++ integrationtest/content/cgi_bin/valid_cgi_case_insensitive_content_type.py
@@ -0,0 +1,13 @@
+import os
+
+# document response
+print("conTent-tyPE: text/plain")
+print("X_TEST00: test")
+print("X_TEST01: test")
+print("X_TEST02: test")
+print("")
+print("Hello, world.")
+print("This is body of CGI.")
+
+for name, value in os.environ.items():
+    print("{0}: {1}".format(name, value))
