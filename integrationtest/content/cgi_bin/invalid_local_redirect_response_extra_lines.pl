4f5b7779f0e660c4af539bbbff31825429337eb2
ShotaTanemura <shota0711tane@gmail.com>
2023-11-12 17:37:26 +0900

    WS08 Add test code for invalid local redirect response with extra lines

--- integrationtest/content/cgi_bin/invalid_local_redirect_response_extra_lines.pl
+++ integrationtest/content/cgi_bin/invalid_local_redirect_response_extra_lines.pl
@@ -1,1 +1,6 @@
-print "Location:/\n\n";
+print "Location: /\n";
+print "Location: https://example.com\n";
+print "Status: 302 Found\n";
+print "Content-Type: text/plain\n";
+print "\n";
+print "This is my CGI Client Redirect Response with Document.";
