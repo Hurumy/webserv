4d5e308a3e5923c3218b086a2a05acb31b9b70b3
ShotaTanemura <shota0711tane@gmail.com>
2023-12-16 21:41:32 +0900

    Add test script for trailer field

---
+++ integrationtest/tests/test_post_request.py
@@ -0,0 +1,30 @@
+import unittest
+import os
+from dotenv import load_dotenv
+from client.Ipv4Client import Ipv4Client
+import requests
+
+load_dotenv()
+HOST_NAME = os.getenv('HOST_NAME')
+PORT = os.getenv('PORT')
+URI_TOP = 'http://' + HOST_NAME + ':' + PORT
+
+class TestPostRequest(unittest.TestCase):
+
+	def test_trailerfield(self):
+		print('\n===========================')
+		print('TEST: test trailerfield Chunked transfer encoding')
+		print('===========================\n')
+
+		with open('./data/chunked_trailer', 'rb') as fs:
+			file_text = fs.read()
+		headers = {
+			'Content-Type':'text/plain',
+			'Transfer-Encoding':'chunked',
+			'Trailer':'Expires'
+        }
+		r = requests.post(URI_TOP, data=file_text, headers=headers)
+		print('Response:\n',r.text)
+		print('Status: ',r.status_code)
+		# self.assertEqual(r.text, file_text)
+		# self.assertEqual(r.status_code, 200)
