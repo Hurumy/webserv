fad41a6c61e14ce19741d1229db65f47a31570ee
ShotaTanemura <shota0711tane@gmail.com>
2023-12-24 11:47:37 +0900

    Refactor test.sh

--- integrationtest/tests/test.sh
+++ integrationtest/tests/test.sh
@@ -1,6 +1,12 @@
+echo '==========================='
+echo 'TEST: test Send Chunked transfer encodede body with trailer field'
+echo '==========================='
+
 curl -X POST -T "./content/wordpress-logo.jpg" -H 'Content-type: image/jpeg' --header "Transfer-Encoding: chunked" --header "Connection: Keep-Alive" http://localhost:8081/
+
 ACTUAL=`cat ./content/wordpress-logo.jpg | wc -c`
 CMP=`cat ./content/0.jpg | wc -c`
+
 rm -rf ./content/0.jpg
 if [ ${ACTUAL} -ne ${CMP} ]; then
 	exit 1
