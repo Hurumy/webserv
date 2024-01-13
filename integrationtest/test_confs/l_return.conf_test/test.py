c444e04c486d79cc4e83285a0b50425c50a4b368
ShotaTanemura <shota0711tane@gmail.com>
2024-01-13 11:31:45 +0900

    Add integration test of l_return

---
+++ integrationtest/test_confs/l_return.conf_test/test.py
@@ -0,0 +1,49 @@
+import unittest
+from dotenv import load_dotenv
+import os
+import requests
+
+load_dotenv()
+HOST_NAME = os.getenv('HOST_NAME')
+PORT = os.getenv('PORT')
+HOST_NAME = "localhost"
+
+class TestLReturnConfigs(unittest.TestCase):
+
+	def test_simple_get_redirect_page(self):
+		print('\n===========================')
+		print('TEST: Simple GET redirect page')
+		print('===========================\n')
+
+		PORT = "8080"
+		URI_TOP = 'http://' + HOST_NAME + ':' + PORT
+
+		with open('./content/readme.html') as fs:
+			file_text = fs.read()
+		headers = {
+			'Host':'def'
+		}
+		r = requests.get(URI_TOP + '/content/a/', headers=headers)
+		print('Response:\n',r.text)
+		print('Status: ',r.status_code)
+		self.assertEqual(r.text, file_text)
+		self.assertEqual(r.status_code, 200)
+
+	def test_having_prior_over_top_scope(self):
+		print('\n===========================')
+		print('TEST: Test having prior over top scope')
+		print('===========================\n')
+
+		PORT = "8081"
+		URI_TOP = 'http://' + HOST_NAME + ':' + PORT
+
+		with open('./content/readme.html') as fs:
+			file_text = fs.read()
+		headers = {
+			'Host':'sec'
+		}
+		r = requests.get(URI_TOP + '/content/a/', headers=headers)
+		print('Response:\n',r.text)
+		print('Status: ',r.status_code)
+		self.assertEqual(r.text, file_text)
+		self.assertEqual(r.status_code, 200)
