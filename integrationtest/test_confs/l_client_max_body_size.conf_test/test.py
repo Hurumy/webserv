83e06201600268572253b798e52bc4b5168af635
ShotaTanemura <shota0711tane@gmail.com>
2024-01-13 10:59:04 +0900

    Add invalid config files for integration test of l_client_max_body_size

---
+++ integrationtest/test_confs/l_client_max_body_size.conf_test/test.py
@@ -0,0 +1,61 @@
+import unittest
+from dotenv import load_dotenv
+import os
+import requests
+
+load_dotenv()
+HOST_NAME = os.getenv('HOST_NAME')
+PORT = os.getenv('PORT')
+HOST_NAME = "localhost"
+PORT = "8080"
+URI_TOP = 'http://' + HOST_NAME + ':' + PORT
+
+class TestLClientMaxBodySizeConfigs(unittest.TestCase):
+
+	def test_post_with_max_body_size(self):
+		print('\n===========================')
+		print('TEST: POST with max body size')
+		print('===========================\n')
+
+		data = 'aaaaa'
+		headers = {
+			'Host':'def',
+			'Content-Type':'text/plain'
+		}
+		r = requests.get(URI_TOP + '/content/a/', data=data, headers=headers)
+		print('Response:\n',r.text)
+		print('Status: ',r.status_code)
+		self.assertEqual(r.status_code, 201)
+		if os.path.exists('./content/a/0.txt'): os.remove('./content/a/0.txt')
+
+	def test_post_with_over_max_body_size(self):
+		print('\n===========================')
+		print('TEST: POST with max body size')
+		print('===========================\n')
+
+		data = 'aaaaaa'
+		headers = {
+			'Host':'def',
+			'Content-Type':'text/plain'
+		}
+		r = requests.get(URI_TOP + '/content/a/', data=data, headers=headers)
+		print('Response:\n',r.text)
+		print('Status: ',r.status_code)
+		self.assertEqual(r.status_code, 413)
+		if os.path.exists('./content/a/0.txt'): os.remove('./content/a/0.txt')
+
+	def test_having_prior_over_top_scope(self):
+		print('\n===========================')
+		print('TEST: Having prior over top scope')
+		print('===========================\n')
+
+		data = 'aaaaa'
+		headers = {
+			'Host':'sec',
+			'Content-Type':'text/plain'
+		}
+		r = requests.get(URI_TOP + '/content/a/', data=data, headers=headers)
+		print('Response:\n',r.text)
+		print('Status: ',r.status_code)
+		self.assertEqual(r.status_code, 201)
+		if os.path.exists('./content/a/0.txt'): os.remove('./content/a/0.txt')
